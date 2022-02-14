815b57c81edb6c0176304ed1ba838b1ffdc3df2a
bjiake <suyd@list.ru>
2022-02-14 11:24:00 +0700

    Добавьте файлы проекта.

---
+++ 1.1.1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Task2 NewOperation = new Task2();
+            NewOperation.WriteResult();
+            int Value = Convert.ToInt32(Console.ReadLine());
+            int Times = Convert.ToInt32(Console.ReadLine());
+            int BuffValue = Value;
+            for (int Iterator = 1; Iterator < Times; ++Iterator)
+            {
+                Value *= BuffValue;
+            }
+            Console.WriteLine(Value);
+            
+        }
+    }
+}
---
+++ 1.1.1/Task2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._1
+{
+    class Task2
+    {
+        public void WriteResult()
+        {
+            int NumberRemove = 1, CountRemove = 1;
+            int value = Convert.ToInt32(Console.ReadLine());
+            if (value < 10)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                string SwitchValue = value.ToString();
+                char CorrectSwitch = SwitchValue[1];
+                SwitchValue = SwitchValue.Remove(NumberRemove, CountRemove);
+                SwitchValue = SwitchValue + CorrectSwitch;
+                Console.WriteLine(SwitchValue);
+            }
+        }
+    }
+}
